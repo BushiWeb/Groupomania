1872116b1d7adc8db53c2b1a13fffc7e8f5eac9a
Emeric <emeric.francisod+github@gmail.com>
2022-11-04 18:33:54 +0100

    Add server utils to improve server start

---
+++ api/src/server.js
@@ -0,0 +1,27 @@
+import http from 'node:http';
+import app from './app.js';
+import { normalizePort, getConnectionInformations, errorHandler } from './utils/server-utils.js';
+
+// Get the port
+let port;
+try {
+    port = normalizePort(process.env.PORT);
+} catch (error) {
+    port = normalizePort('3000');
+    console.warn('No port value in environment variable, use default port 3000 instead.');
+}
+
+app.set('port', port);
+
+// Create the server
+const server = http.createServer(app);
+
+// Add event listeners
+server.on('error', errorHandler);
+server.on('listening', () => {
+    const connectionInformations = getConnectionInformations(server, port);
+    console.log(`Server listening on ${connectionInformations}`);
+});
+
+// Launch the server
+server.listen(port);
