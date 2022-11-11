094ae6abbca22eb821183b3b8a91399af2b3270b
Emeric Francisod <emeric.francisod+github@gmail.com>
2022-11-11 11:14:58 +0100

    Add global toml configuration file

--- api/src/config/config.js
+++ api/src/config/config.js
@@ -1,4 +1,11 @@
 import convict from 'convict';
+import toml from '@ltd/j-toml';
+import {dirname, join} from 'node:path';
+import {fileURLToPath} from 'node:url';
+
+
+// Add TOML parser to Convict
+convict.addParser({extension: 'toml', parse: (source) => toml.parse(source, {joiner: '\n', bigint: false})});
 
 // Define configuration schema
 let config = convict({
@@ -13,8 +20,11 @@ let config = convict({
         format: 'port',
         default: 3000,
         env: 'PORT'
-    },
+    }
 });
+
+// Import configuration files
+config.loadFile(join(dirname(fileURLToPath(import.meta.url)), 'config.toml'));
 
 // Validate configuration
 config.validate({allowed: 'strict'});
