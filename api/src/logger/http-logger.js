d52617f36221ebe573448b91124f94040ed49832
Emeric Francisod <emeric.francisod+github@gmail.com>
2022-11-16 16:26:45 +0100

    feat: morgan middleware for http logs

---
+++ api/src/logger/http-logger.js
@@ -0,0 +1,15 @@
+import morgan from 'morgan';
+import createLoggerNamespace from './logger-namespace.js';
+
+// Log Morgan messages using a custom logger
+const httpLogger = createLoggerNamespace('groupomania:api:http');
+
+const stream = {
+    write: (message) => httpLogger.http(message)
+};
+
+const logFormat = ':remote-addr - :remote-user [:date[web]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" - :response-time ms';
+
+const morganMiddleware = morgan(logFormat, {stream});
+
+export default morganMiddleware;
