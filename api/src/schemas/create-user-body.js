eb5037bbbcc0da75322bb3af6cc3671680faa952
Emeric <emeric.francisod+github@gmail.com>
2022-12-13 09:31:03 +0100

    refactor: refactor schemas
    Split each schema into atomic and configurable parts, like email or
    password.
    Allows for reuse of those parts.

--- api/src/schemas/create-user-body.js
+++ api/src/schemas/create-user-body.js
@@ -1,6 +1,5 @@
-import config from '../config/config.js';
-
-const passwordConfiguration = config.get('password');
+import generateEmailSchema from './atoms/email.js';
+import generatePasswordSchema from './atoms/password.js';
 
 /**
  * Schema for the create user request's body.
@@ -8,36 +7,8 @@ const passwordConfiguration = config.get('password');
  * Contains a required, strong password. Password strength can be changed in the configuration.
  */
 const createUserBodySchema = {
-    email: {
-        in: ['body'],
-        exists: {
-            errorMessage: 'The email is required.',
-            options: {
-                checkFalsy: true
-            },
-            bail: true
-        },
-        trim: true,
-        isEmail: {
-            errorMessage: 'The email must have the right format. It must contain your email username, followed by an @, followed by a domain name.',
-            bail: true
-        }
-    },
-
-    password: {
-        in: ['body'],
-        exists: {
-            errorMessage: 'The password is required.',
-            options: {
-                checkFalsy: true
-            },
-            bail: true
-        },
-        isStrongPassword: {
-            errorMessage: `The password must be stronger. It has to contain at least ${passwordConfiguration.minLength} characters, including at least ${passwordConfiguration.minLowercase} lowercase letters, ${passwordConfiguration.minUppercase} uppercase letters, ${passwordConfiguration.minNumbers} numbers and ${passwordConfiguration.minSymbols} symbols.`,
-            options: passwordConfiguration
-        }
-    }
+    email: generateEmailSchema(),
+    password: generatePasswordSchema()
 };
 
 export default createUserBodySchema;
--- api/src/schemas/login.js
+++ api/src/schemas/login.js
@@ -1,34 +1,14 @@
+import generateEmailSchema from './atoms/email.js';
+import generatePasswordSchema from './atoms/password.js';
+
 /**
  * Schema for the login request's body.
  * Contains a required email.
  * Contains a required password.
  */
 const loginBodySchema = {
-    email: {
-        in: ['body'],
-        exists: {
-            errorMessage: 'The email is required.',
-            options: {
-                checkFalsy: true
-            },
-            bail: true
-        },
-        trim: true,
-        isEmail: {
-            errorMessage: 'The email must have the right format. It must contain your email username, followed by an @, followed by a domain name.',
-            bail: true
-        }
-    },
-
-    password: {
-        in: ['body'],
-        exists: {
-            errorMessage: 'The password is required.',
-            options: {
-                checkFalsy: true
-            }
-        }
-    }
+    email: generateEmailSchema(),
+    password: generatePasswordSchema({ checkStrength: false })
 };
 
 export default loginBodySchema;
