2e1ca154b928213da268775b33a1867c11d7a59b
Emeric <emeric.francisod+github@gmail.com>
2022-12-24 23:10:45 +0100

    fix: Get all users works with filter and more info

--- api/src/services/posts.js
+++ api/src/services/posts.js
@@ -19,3 +19,68 @@ export async function createPost(postInfos) {
 
     return newPost;
 }
+
+
+
+/**
+ * Fetch all posts, ordered by creation date.
+ * @param {Object} options - Search options.
+ * @param {boolean} [options.userInfo=false] - Whether to add the user informations to the posts or just the author id.
+ * @param {boolean} [options.likeInfo=false] - Whether to add the like informations to the posts.
+ * @param {string|number} [options.userId] - Filtering posts by userId.
+ * @param {number} [option.page] - Which page to get, usefull for pagination. Requires the limit parameter.
+ * @param {number} [option.limit] - Number of entry to return, usefull for pagination.
+ * @returns {User} Returns the user.
+ */
+export async function getAllPosts({ userInfo = false, likeInfo = false, userId, page, limit } = {}) {
+    postsServicesLogger.verbose('Get all posts service starting');
+
+    let searchOptions = {
+        attributes: ['postid', 'title', 'message', 'imageUrl', 'creationDate', 'lastUpdateDate', 'writerId'],
+        order: [['creationDate', 'DESC']]
+    };
+
+    // Creating user filter
+    if (userId !== undefined) {
+        postsServicesLogger.debug('Filtering by user id');
+        searchOptions.where = {
+            writerId: userId
+        };
+    }
+
+    // Adding user informations
+    if (userInfo) {
+        postsServicesLogger.debug('Adding user informations to the results');
+        searchOptions.include = {
+            association: 'writer',
+            attributes: [['userId', 'writerId'], 'email', 'roleId']
+        };
+        searchOptions.attributes.pop();
+    }
+
+    // Adding likes informations
+    if (likeInfo) {
+        postsServicesLogger.debug('Adding like informations to the results');
+        searchOptions.include = {
+            association: 'writer',
+            attributes: [['userId', 'writerId'], 'email', 'roleId']
+        };
+        searchOptions.attributes.pop();
+    }
+
+    // Adding pagination informations
+    if (limit) {
+        postsServicesLogger.debug(`Limiting the number of results to ${limit}`);
+        searchOptions.limit = limit;
+    }
+
+    if (limit && page) {
+        postsServicesLogger.debug(`Paginating the result, getting the page number ${page}`);
+        searchOptions.offset = (page - 1) * limit;
+    }
+
+    const users = await db.models.User.findAll(searchOptions);
+
+    postsServicesLogger.debug('Users fetched');
+    return users;
+}
