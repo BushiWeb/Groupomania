28562bb9e1a59bad0d86d5fd04573671fa0d44ab
Emeric <emeric.francisod+github@gmail.com>
2023-04-18 10:16:31 +0200

    refactor: update state layer method
    Use pseudo-element instead of gradient to simplify things.

--- frontend/src/components/HiddenNavigationLink/HiddenNavigationLink.test.js
+++ frontend/src/components/HiddenNavigationLink/HiddenNavigationLink.test.js
@@ -1,6 +1,7 @@
 import HiddenNavigationLink from './HiddenNavigationLink';
 import { screen } from '@testing-library/react';
 import { render } from '../../utils/test-wrapper';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 
 describe('HiddenNavigationLink component test suite', () => {
@@ -14,5 +15,18 @@ describe('HiddenNavigationLink component test suite', () => {
     it('should have the right label', () => {
         render(<HiddenNavigationLink label={label} target={target} />);
         screen.getByRole('link', { name: label });
+    });
+
+    it('should be accessible using the keyboard', async () => {
+        const user = userEvent.setup();
+
+        render(<HiddenNavigationLink label={label} target={target} />);
+        const linkElt = screen.getByRole('link', { name: label });
+
+        expect(linkElt).not.toHaveFocus();
+
+        await user.tab();
+
+        expect(linkElt).toHaveFocus();
     });
 });
