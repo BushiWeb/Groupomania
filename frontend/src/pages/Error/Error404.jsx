ca7e722c1d53146cbcdccc2ebd3134eee3a119a7
Emeric Francisod <emeric.francisod+github@gmail.com>
2023-11-13 22:56:38 +0100

    fix: fix error styling

--- frontend/src/pages/Error/Error404.jsx
+++ frontend/src/pages/Error/Error404.jsx
@@ -11,23 +11,23 @@ export const PAGE_NAME = 'User';
 export default function Error404() {
     useChangePageTitle('Groupomania - 404');
 
-    return <div className={style.notFound}>
-        <h1 className={style.heading}>Nous n'avons pas trouvé cette page.</h1>
-        <p className={style.message}>
-            Si vous êtes sûr d'être à la bonne adresse, c'est que cette page n'existe pas encore.
-        </p>
-
-        <div className={style.ctaWrapper}>
-            <h2 className={style.question}>Vous n'êtes pas bien ici ?</h2>
-
-            <InteractiveElement
-                rootElement={Link}
-                rippleDuration={350}
-                stateLayerColor='on-primary'
-                to="/"
-                className={style.cta}>
-                Retourner à la page d'accueil
-            </InteractiveElement>
+    return <div className={style.container}>
+        <div className={style.notFound}>
+            <h1 className={style.heading}>Nous n'avons pas trouvé cette page.</h1>
+            <p className={style.message}>
+                Si vous êtes sûr d'être à la bonne adresse, c'est que cette page n'existe pas encore.
+            </p>
+            <div className={style.ctaWrapper}>
+                <h2 className={style.question}>Vous n'êtes pas bien ici ?</h2>
+                <InteractiveElement
+                    rootElement={Link}
+                    rippleDuration={350}
+                    stateLayerColor='on-primary'
+                    to="/"
+                    className={style.cta}>
+                    Retourner à la page d'accueil
+                </InteractiveElement>
+            </div>
         </div>
     </div>;
 }
